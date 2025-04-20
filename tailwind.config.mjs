59a94cde78bc8e10242a2b5f3c3afb7f7b8f6c0e
Mario Morales <mandres0807@outlook.com>
2025-04-20 10:57:25 -0500

    added custom font: camingoCode, setting some fonts styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,52 +1,56 @@
 function withOpacity(variableName) {
-  return ({ opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(var(${variableName}), ${opacityValue})`;
-    }
-    return `rgb(var(${variableName}))`;
-  };
+   return ({ opacityValue }) => {
+      if (opacityValue !== undefined) {
+         return `rgba(var(${variableName}), ${opacityValue})`;
+      }
+      return `rgb(var(${variableName}))`;
+   };
 }
 
 export default {
-  darkMode: "class",
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      textColor: {
-        skin: {
-          base: withOpacity("--color-text-base"),
-          muted: withOpacity("--color-text-muted"),
-          inverted: withOpacity("--color-text-inverted"),
-        },
+   darkMode: "class",
+   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+   theme: {
+      extend: {
+         fontFamily: {
+            mono: ['Camingo Code', 'monospace'],
+            camingo: ['Camingo Code', 'monospace']
+         },
+         textColor: {
+            skin: {
+               base: withOpacity("--color-text-base"),
+               muted: withOpacity("--color-text-muted"),
+               inverted: withOpacity("--color-text-inverted"),
+            },
+         },
+         backgroundColor: {
+            skin: {
+               fill: withOpacity("--color-fill"),
+               "button-accent": withOpacity("--color-button-accent"),
+               "button-accent-hover": withOpacity("--color-button-accent-hover"),
+               "button-muted": withOpacity("--color-button-muted"),
+            },
+         },
+         colors: {
+            skin: {
+               hue: withOpacity("--color"),
+               muted: withOpacity("--muted"),
+            },
+         },
+         ringColor: {
+            skin: {
+               fill: withOpacity("--color-fill"),
+            },
+         },
+         gradientColorStops: {
+            skin: {
+               hue: withOpacity("--color-fill"),
+            },
+         },
       },
-      backgroundColor: {
-        skin: {
-          fill: withOpacity("--color-fill"),
-          "button-accent": withOpacity("--color-button-accent"),
-          "button-accent-hover": withOpacity("--color-button-accent-hover"),
-          "button-muted": withOpacity("--color-button-muted"),
-        },
-      },
-      colors: {
-        skin: {
-          hue: withOpacity("--color"),
-          muted: withOpacity("--muted"),
-        },
-      },
-      ringColor: {
-        skin: {
-          fill: withOpacity("--color-fill"),
-        },
-      },
-      gradientColorStops: {
-        skin: {
-          hue: withOpacity("--color-fill"),
-        },
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+   },
+   variants: {
+      extend: {},
+   },
+   plugins: [],
 };
